e62c6e8d822247d1571aa0dec66de3181be63eb5
mintychochip <jlo2@csub.edu>
2026-02-21 09:04:23 +0000

    summary statistics

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
@@ -13,13 +12,49 @@ forums](https://discuss.streamlit.io).
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+url = 'https://raw.githubusercontent.com/Noah-Gallego/Plot-A-Thon/main/gamer_study_raw.csv'
+df = pd.read_csv(url)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+generations = {
+    'Gen Z': 'Z',
+    'Boomer': 'B',
+    'Millennial': 'M',
+    'Gen X': 'X',
+    'Silent': 'S'
+}
 
+genders = {
+    'Female': 'F',
+    'Male': 'M',
+    'Nonbinary/Other': 'N',
+    'MALE': 'M',
+    'femal': 'F'
+}
+
+races = {
+    'White/Caucasian': 'W',
+    'Hispanic': 'H',
+    'Black/African American': 'B',
+    'Other': 'O',
+    'Asian/Pacific Islander': 'A',
+    'White': 'W',
+    'Native American/Alaskan': 'N'
+}
+
+lgbt_identifications = {
+    'Prefer not to say': 2,
+    'Yes': 1,
+    'No': 0
+}
+df['is_player'] = df['is_player'].map(lambda x: True if x == 'TRUE' or x == 'Yes' else False)
+df = df.apply(lambda x: x.astype(int) if x.dtype == 'bool' else x)
+df = df.apply(lambda x: x.map(generations) if x.name == 'generation' else x)
+df = df.apply(lambda x: x.map(genders) if x.name == 'gender' else x)
+df = df.apply(lambda x: x.map(races) if x.name == 'race_ethnicity_primary' else x)
+df = df.apply(lambda x: x.map(lgbt_identifications) if x.name == 'lgbt_identification' else x)
+df['age'] = df['age'].map(lambda x: x if x > 0 else 0)
+st.dataframe(df.describe())
+# 
 x = radius * np.cos(theta)
 y = radius * np.sin(theta)
 
@@ -38,3 +73,6 @@ st.altair_chart(alt.Chart(df, height=700, width=700)
         color=alt.Color("idx", legend=None, scale=alt.Scale()),
         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
     ))
+
+# 
+# forest 
